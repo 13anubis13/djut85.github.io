bdd29cf7277dc1eb9b70bba7f09ebdf6322ab83c
djut85 <rex.brown@tut.by>
2017-05-31 16:10:41 +0300

    First commit

---
+++ first/js/script.js
@@ -0,0 +1,38 @@
+$('document').ready(function(){
+    $('.jcarousel').jcarousel({
+            // Configuration goes here
+    });
+    // Previous and next controls
+    $('.jcarousel-prev')
+        .on('jcarouselcontrol:active', function() {
+            $(this).removeClass('inactive');
+        })
+        .on('jcarouselcontrol:inactive', function() {
+            $(this).addClass('inactive');
+        })
+        .jcarouselControl({
+            target: '-=1'
+        });
+
+        $('.jcarousel-next')
+        .on('jcarouselcontrol:active', function() {
+            $(this).removeClass('inactive');
+        })
+        .on('jcarouselcontrol:inactive', function() {
+            $(this).addClass('inactive');
+        })
+        .jcarouselControl({
+            target: '+=1'
+        });
+    // Pagination 
+    $('.jcarousel-pagination')
+        .on('jcarouselpagination:active', 'a', function() {
+            $(this).addClass('active');
+        })
+        .on('jcarouselpagination:inactive', 'a', function() {
+            $(this).removeClass('active');
+        })
+        .jcarouselPagination();
+    
+    //alert("Everything's cool!");
+});
